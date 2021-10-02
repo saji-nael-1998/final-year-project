ae2d7757a224db59da28b679c299d39b469c4db7
saji-nael <rockyzew@gmail.com>
2021-10-03 02:26:59 +0300

    first-project

--- controller/Login.php
+++ controller/Login.php
@@ -1,7 +1,6 @@
 <?php
 
 session_start();
-
 require_once('../model/admin.php');
 class LoginContoller
 {
--- js/login.js
+++ js/login.js
@@ -1,19 +1,34 @@
-$('#login-form').submit(function (event) {
-  event.preventDefault();
-  //fetch values from the form fields
-  email = $('#inputEmail').val();
-  password = $('#inputPassword').val();
-  //prepare data for get request
-  let data = "email=" + email + "&" + "password=" + password;
-  //create ajax request 
-  $.ajax(
-    {
-      type: 'GET',
-      url: '../controller/Login.php',
-      data: data,
-      success: function (data) {
-        alert(data);
+var isLogged =sessionStorage.getItem("isLogged");
+if (!isLogged) {
+  location.replace("https://www.w3schools.com")
+}else{
+  function saveUser(email, password) {
+    // Store
+    sessionStorage.setItem("email", email);
+    sessionStorage.setItem("isLogged", true);
+  }
+  $('#login-form').submit(function (event) {
+    event.preventDefault();
+    //fetch values from the form fields
+    email = $('#inputEmail').val();
+    password = $('#inputPassword').val();
+    //prepare data for get request
+    let data = "email=" + email + "&" + "password=" + password;
+    //create ajax request 
+    $.ajax(
+      {
+        type: 'GET',
+        url: '../controller/Login.php',
+        data: data,
+        success: function (data) {
+          if (data === '1') {
+            saveUser(email);
+          } else {
+            alert('sasssji');
+          }
+        }
       }
-    }
-  );
-});
+    );
+  });
+}
+
